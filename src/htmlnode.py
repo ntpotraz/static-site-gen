413448f7cc80ba167b6819e6cb34f3770c0e1136
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-05-23 15:44:49 -0700

    Finshed parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,4 +20,29 @@ class HTMLNode:
         return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
     
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError()
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None or self.tag == "":
+            raise ValueError("parent node needs a tag")
+        if self.children is None or self.children == "":
+            raise ValueError("parent node needs child nodes")
+        output = ""
+        for child in self.children:
+            output += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{output}</{self.tag}>"
